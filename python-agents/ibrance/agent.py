851236ec0114bbfebf09d84e8ec3f77a978b6f1f
praveenbandlazs <praveen.bandla@zs.com>
2025-08-14 13:45:25 +0000

    test change

--- python-agents/ibrance/agent.py
+++ python-agents/ibrance/agent.py
@@ -4,6 +4,6 @@ class IbranceAgent(BaseLLM):
     def process(self, query, settings, trace):
         user_input = query["messages"][0]["content"].lower()
         if "net sales" in user_input:
-            return {"text": "the net sales for Ibrance is $1,200,000"}
+            return {"text": "the net sales for Ibrance is $2,200,000"}
         else:
             return {"text": "unknown query for Ibrance insights"}
