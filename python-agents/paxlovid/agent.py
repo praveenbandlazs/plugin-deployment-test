21beef52edc0b74ee6ebabc0c584031f06bbe3a1
praveenbandlazs <praveen.bandla@zs.com>
2025-08-14 12:44:00 +0000

    test change

--- python-agents/paxlovid/agent.py
+++ python-agents/paxlovid/agent.py
@@ -4,6 +4,6 @@ class PaxlovidAgent(BaseLLM):
     def process(self, query, settings, trace):
         user_input = query["messages"][0]["content"].lower()
         if "net sales" in user_input:
-            return {"text": "the net sales for paxlovid is $2,000,000"}
+            return {"text": "the net sales for paxlovid is $2,200,000"}
         else:
             return {"text": "unknown query for paxlovid insights"}
